27087a09594a46bbaa53b47fea9add1fc51769ae
Stephen Roberts <stephen.roberts@anu.edu.au>
2021-08-15 19:42:00 +1000

    update file sep and print

--- RunMerewether_Rain.py
+++ RunMerewether_Rain.py
@@ -6,8 +6,8 @@ Pasha Bulka Storm Event 2007 - storm burst model
 with option to run the entire 2 day event if you are not time limited
 """
 
-import anuga, anuga.parallel, numpy, time, os, glob
-from anuga.operators.rate_operators import Polygonal_rate_operator
+import anuga, numpy, time, os, glob, shutil
+from anuga import Rate_operator
 from anuga import file_function, Polygon_function, read_polygon, create_mesh_from_regions, Domain
 from anuga import distribute, myid, numprocs, finalize, barrier
 
@@ -15,12 +15,15 @@ basename = 'Model/DEM/M'
 outname = '2007'
 meshname = 'Model/DEM/M.msh'
 
-if anuga.parallel.myid == 0:
+if anuga.myid == 0:
     
-    CatchmentDictionary = {'Model/Bdy/catchment.csv':25., 'Model/Bdy/fine.csv':1.}
+    # First untar the large DEM file
+    shutil.unpack_archive(os.path.join('Model','DEM','M.csv.tgz'), os.path.join('Model','DEM'))
+
+    CatchmentDictionary = {os.path.join('Model','Bdy','catchment.csv'):25., os.path.join('Model','Bdy','fine.csv'):1.}
     
-    bounding_polygon = anuga.read_polygon('Model/Bdy/catchment.csv')
-    interior_regions = anuga.read_polygon_dir(CatchmentDictionary, 'Model/Bdy')
+    bounding_polygon = anuga.read_polygon(os.path.join('Model','Bdy','catchment.csv'))
+    interior_regions = anuga.read_polygon_dir(CatchmentDictionary, os.path.join('Model','Bdy'))
     
     BuildingDictionary = 'Model/Houses/houses.csv'
     interior_holes = anuga.read_multi_poly_file(BuildingDictionary)
@@ -34,12 +37,12 @@ if anuga.parallel.myid == 0:
         verbose=True)
  
     domain = Domain(meshname, use_cache=True, verbose=True)
-    print 'reading terrain grid'
+    print ('reading terrain grid')
     domain.set_quantity('elevation', filename=basename+'.csv', use_cache=True, verbose=True, alpha=0.99)
     domain.set_name(outname)
-    print domain.statistics()
+    print (domain.statistics())
 
-    print 'Applying mannings roughness'
+    print ('Applying mannings roughness')
     Roughness_Polygons_directory = 'Model/Mannings'
     friction_list = anuga.get_polygon_value_list(Roughness_Polygons_directory)
     domain.set_quantity('friction', anuga.Polygon_function(friction_list, default=0.02, geo_reference=domain.geo_reference))
@@ -49,22 +52,22 @@ else:
 	domain = None
 
 verbose = True
-if anuga.parallel.myid == 0 and verbose: print 'DISTRIBUTING DOMAIN'
+if anuga.myid == 0 and verbose: print ('DISTRIBUTING DOMAIN')
 domain = anuga.parallel.distribute(domain)
 domain.set_minimum_storable_height(0.05)
-if anuga.parallel.myid == 0 and verbose: print 'CREATING INLETS'  
+if anuga.myid == 0 and verbose: print ('CREATING INLETS')  
 
 #change from "rain_burst.tms" to "rain_entire_storm.tms" to run the 2 day event
 polygon = anuga.read_polygon('Model/Rain/Gauge/catchment.csv')
 rainfall = anuga.file_function('Model/Rain/Rainfall/rain_burst.tms', quantities='rate')
-op1 = Polygonal_rate_operator(domain, rate=rainfall, factor=1.0e-3, polygon=polygon, default_rate = 0.0) 
+op1 = Rate_operator(domain, rate=rainfall, factor=1.0e-3, polygon=polygon, default_rate = 0.0) 
 
-print 'Available boundary tags', domain.get_boundary_tags()
+print ('Available boundary tags', domain.get_boundary_tags())
 Br = anuga.Reflective_boundary(domain)
 Bd = anuga.Dirichlet_boundary([0,0,0])
 domain.set_boundary({'interior': Br, 'exterior': Bd, 'west': Bd, 'south': Bd, 'north': Bd, 'east': Bd})
 
-if anuga.parallel.myid == 0 and verbose: print 'EVOLVE'
+if anuga.parallel.myid == 0 and verbose: print ('EVOLVE')
 t0 = time.time()
 
 #change final time from 46500. to 177600. to run the 2 day event
@@ -73,11 +76,11 @@ for t in domain.evolve(yieldstep = 300., finaltime = 46500.):
         domain.write_time()
 
 if anuga.parallel.myid == 0:
-    print 'Number of processors %g ' %anuga.parallel.numprocs
-    print 'That took %.2f seconds' %(time.time()-t0)
-    print 'Communication time %.2f seconds'%domain.communication_time
-    print 'Reduction Communication time %.2f seconds'%domain.communication_reduce_time
-    print 'Broadcast time %.2f seconds'%domain.communication_broadcast_time
+    print ('Number of processors %g ' %anuga.parallel.numprocs)
+    print ('That took %.2f seconds' %(time.time()-t0))
+    print ('Communication time %.2f seconds'%domain.communication_time)
+    print ('Reduction Communication time %.2f seconds'%domain.communication_reduce_time)
+    print ('Broadcast time %.2f seconds'%domain.communication_broadcast_time)
     
 domain.sww_merge(delete_old=True)
-anuga.parallel.finalize()
+anuga.finalize()
